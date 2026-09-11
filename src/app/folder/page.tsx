63ab4fb4b62c1dae275e75ae7dfffe5addc74664
defnegenc <64100112+defnegenc@users.noreply.github.com>
2026-09-11 03:25:16 -0400

    Create page.tsx

---
+++ src/app/folder/page.tsx
@@ -0,0 +1,7 @@
+import { FolderUnravel } from "@/components/folder-unravel";
+
+export const metadata = { title: "folder" };
+
+export default function FolderPage() {
+  return <FolderUnravel />;
+}
